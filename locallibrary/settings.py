4f1455c943ff865ce701588db29d0de8fa33fc7a
Altai <busurmankulov.altay@mail.ru>
2019-08-27 15:34:46 +0300

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2tf7&i^&@29wnzhc04vsg9$unx5ys46chzu*o62lfl$&vpca3o'
+# SECRET_KEY = '2tf7&i^&@29wnzhc04vsg9$unx5ys46chzu*o62lfl$&vpca3o'
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
